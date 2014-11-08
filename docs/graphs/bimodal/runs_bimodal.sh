54fb2c28880bd6f8402bafb97375b57769758e47
Aravindhan Dhanasekaran <adhanas@ncsu.edu>
2014-11-08 10:27:17 -0500

    Added gshare runs' data

---
+++ docs/graphs/bimodal/runs_bimodal.sh
@@ -0,0 +1,21 @@
+./sim bimodal 7 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_7.v
+./sim bimodal 8 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_8.v
+./sim bimodal 9 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_9.v
+./sim bimodal 10 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_10.v
+./sim bimodal 11 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_11.v
+./sim bimodal 12 0 0 gcc_trace.txt > ../docs/graphs/bimodal/ad_bi_gcc_12.v
+
+./sim bimodal 7 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_7.v
+./sim bimodal 8 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_8.v
+./sim bimodal 9 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_9.v
+./sim bimodal 10 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_10.v
+./sim bimodal 11 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_11.v
+./sim bimodal 12 0 0 jpeg_trace.txt > ../docs/graphs/bimodal/ad_bi_jpeg_12.v
+
+./sim bimodal 7 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_7.v
+./sim bimodal 8 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_8.v
+./sim bimodal 9 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_9.v
+./sim bimodal 10 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_10.v
+./sim bimodal 11 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_11.v
+./sim bimodal 12 0 0 perl_trace.txt > ../docs/graphs/bimodal/ad_bi_perl_12.v
+
